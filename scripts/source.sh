15fddea09eb6f72897d691f8772005387d633acd
Yann Bernard <yannbernard9@icloud.com>
2023-06-19 23:21:56 +0200

    some fixes

---
+++ scripts/source.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Sourcing envs:";
+export IDF_PATH=/home/devuser/tools/esp-idf;
+source /home/devuser/tools/connectedhomeip/scripts/activate.sh;
+source /home/devuser/tools/esp-idf/export.sh;
